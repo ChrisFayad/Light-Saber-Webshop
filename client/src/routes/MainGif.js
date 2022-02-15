a6cc49734126abfd3b2f6e2842f6e947a3d1b0a9
Chris <fayadchristina@gmail.com>
2022-02-15 15:54:43 +0100

    Add the main route

---
+++ client/src/routes/MainGif.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function MainGif() {
+  return (
+    <div className="locate-gif">
+      <img
+        src="https://media.giphy.com/media/6kQaSUiEXi5X2/giphy.gif"
+        alt="Lightsaber-Color-Guide"
+      />
+      <img
+        src="https://media.giphy.com/media/r09nlclQEzDB6R2mpR/giphy.gif"
+        alt="Lightsaber-Carrier"
+      />
+    </div>
+  );
+}
